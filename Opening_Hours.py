08a51795e59fc751a9132705d24ca43dd59a1c55
eytanbab <73233191+eytanbab@users.noreply.github.com>
2020-11-29 11:49:45 +0200

    Add files via upload

---
+++ Opening_Hours.py
@@ -0,0 +1,16 @@
+
+def opening_hours(store_name):
+    if store_name == 'ZARA':
+        print('Opening hours of ' + store_name + ':')
+        print('8:00 - 21:00')
+    elif store_name == 'Bershka':
+        print('Opening hours of ' + store_name + ':')
+        print('9:00 - 21:00')
+    elif store_name == 'MANGO':
+        print('Opening hours of ' + store_name + ':')
+        print('8:00 - 21:00')
+    else:
+        print('Store not found')
+
+
+
