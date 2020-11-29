b9f7193abc4951baab581f30ddbe9a0c0d4aac03
yuval1121 <56655907+yuval1121@users.noreply.github.com>
2020-11-29 18:21:21 +0200

    Added module 2

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import Opening_Hours
 import module1
 import change_num_of_people
+import module2
+
 
 def main():
     Opening_Hours.opening_hours()
     module1.print_data()
     change_num_of_people.change_num_of_people()
+    module2.START()
+    
     
 
 if __name__ == "__main__":
