19e2a75cfeb111fbeabaf6661ba110c13d69e85e
yuval1121 <56655907+yuval1121@users.noreply.github.com>
2020-11-29 13:02:26 +0200

    Changed main

--- Opening_Hours.py
+++ Opening_Hours.py
@@ -3,6 +3,8 @@ import module1
 num_of_stores = 3
 
 
+obj = module1.open_data()
+
 def opening_hours():
     for i in range(num_of_stores):
         print('Opening hours of ' + obj['Stores'][i]['Name'] + ':')
