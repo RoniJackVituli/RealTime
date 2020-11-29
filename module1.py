4a98fca8fdc70c020274d9a3fcf0afa55fd9660f
yuval1121 <56655907+yuval1121@users.noreply.github.com>
2020-11-29 16:05:05 +0200

    small fixes

--- module1.py
+++ module1.py
@@ -8,13 +8,14 @@ def open_data():
 
 
 def print_data():
+	
 	obj = open_data()
-
+	print()
 	print("Stores:")
 	print()
 	for i in range(3):
 		print(obj["Stores"][i]["Name"])
-		print("Working hours", obj["Stores"][i]["Hours"])
+		print("Working hours: ", obj["Stores"][i]["Hours"])
 		print("Amount of people allowed: ",obj["Stores"][i]["Allowed_People"])
 		print(obj["Stores"][i]["Product_types"])
 		print()
