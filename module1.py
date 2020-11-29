18e742d6cc2a3bf073fbd21e09c36757e11cd20d
yuval1121 <56655907+yuval1121@users.noreply.github.com>
2020-11-29 12:28:51 +0200

    Added function call

--- module1.py
+++ module1.py
@@ -1,7 +1,9 @@
 import json
 
-with open('data.json','r') as f:
-	data = f.read()
-obj = json.loads(data)
+def open_data():
+	with open('data.json','r') as f:
+		data = f.read()
+	obj = json.loads(data)
+	return obj
 
 #print(obj['Stores'])
