a4c290642b1f154127ec145bc3909d566bbe32b8
eytanbab <73233191+eytanbab@users.noreply.github.com>
2020-11-29 12:43:19 +0200

    Add files via upload

---
+++ Opening_Hours.py
@@ -0,0 +1,23 @@
+import json
+
+with open('data.json', 'r') as f:
+    data = f.read()
+obj = json.loads(data)
+
+
+def opening_hours(store_name):
+    if store_name == 'ZARA':
+        print('Opening hours of ' + store_name + ':')
+        print((obj['Stores'][0]['Hours']))
+    elif store_name == 'Pull & Bear':
+        print('Opening hours of ' + store_name + ':')
+        print((obj['Stores'][1]['Hours']))
+    elif store_name == 'H&M':
+        print('Opening hours of ' + store_name + ':')
+        print((obj['Stores'][2]['Hours']))
+    else:
+        print('Store not found')
+
+
+store = input('Please enter the name of the store: ')
+opening_hours(store)
