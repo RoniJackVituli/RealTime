26ccfa0d98f69d314173fd883d836740c96b19b7
RoniJackVituli <vitulironi5@gmail.com>
2020-11-29 17:40:01 +0200

    add my files

--- main.py
+++ main.py
@@ -1,45 +1,65 @@
-name_company = "RealTime"
-name_owner = "Roni Jack Vituli"
-StrPassword = "5t4r3e2w1q"
+import json
+from datetime import datetime
+with open('data.json','r') as f:
+    data = f.read()
+obj = json.loads(data)
 
-def update_activity_time():
-
-def check_password(strPass):
-    if password == strPass:
-        return True
+def check_account():
+    for i in range (len(obj['Managers'])):
+        if(obj['Managers'][i]['Name'] == account and obj['Managers'][i]['Password'] == password and obj['Managers'][i]['Store'] == store):
+         return True
     return False
 
-def check(str):
-    with open('Test.txt') as f:
-        if str in f.read():
-            return True
-    return False
+def update_open_hours():
+    print("The correct hours is: ",store['Hours_Open'])
+    store['Hours_Open'] = input('Enter new open Houres: ')
 
-def switch_demo(argument):
+def menu_option(x):
     switcher = {
-        1: ""
+        0: update_open_hours,
     }
+    return switcher.get(x, lambda: "invalid choice")
 
-def check_user_company(user,company):
-    if check(user) and check(company):
-        return True
-    return False
 
-flag = False
+def Take_info_stroe():
+    global store
+    for i in range(len(obj['Stores'])):
+        if(obj['Stores'][i]['Name'] == store):
+            store = obj['Stores'][i]
 
-while(not flag):
-    user_name = input('Enter your name: ')
-    company = input('Enter the name of the company you work: ')
-    if(check_user_company(user_name,company)):
-        flag = True
+def print_menu():
+    global flag
+    flag = False
+    Take_info_stroe()
+    hour = datetime.now().hour
+    choice = 1
+    while (not flag and choice != 0):
+        print("\n\nֿ--------------------Hey {0}, Welcome to the management area--------------------".format(account))
+        print("1. Change opening hours press -> 1")
+        print("2. Exit from the programming -> 0")
+        choice = int(input())
+        menu_option(choice - 1)()
+    if hour >= 5 and hour <= 12:
+        print("All changes have been made\nGood Morning, {0}".format(account))
+    elif hour > 12 and hour < 17:
+        print("All changes have been made\nGood After noon, {0}".format(account))
     else:
-        print("Worng input please try again")
+        print("All changes have been made\nGood Evening, {0}".format(account))
 
-flag = False
-while(not flag):
-    password = input("Hey {0}, Please enter your password: ".format(user_name))
-    if(check_password(password)):
-        flag = True
-    else:
-        print("Worng Password, please enter again")
+def START():
+    print("--------------------Welcome to RealTime--------------------")
+    flag = False
+    store = input('Enter the name of the store: ')
+    account = input('Enter your name: ')
+    password = input('Enter your password: ')
+    while(not check_account()):
+        print('Worng input please try again')
+        store = input('Enter the name of the store: ')
+        account = input('Enter your name: ')
+        password = input('Enter your password: ')
+    print_menu()
+
+
+START()
+
 
