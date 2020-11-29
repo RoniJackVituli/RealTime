07a7108662bee0d93a803a374e948dc9f4b05105
yuval1121 <56655907+yuval1121@users.noreply.github.com>
2020-11-29 17:23:34 +0200

    fixed

--- change_num_of_people.py
+++ change_num_of_people.py
@@ -4,15 +4,13 @@ with open('data.json', 'r') as f:
     data = f.read()
 obj = json.loads(data)
 
-print(obj['Stores'])
-
-Stores = json.opendata()
 
 def change_num_of_people():
     name = input('Enter name of store')
-    for i in range (3):
-        if Stores[i]["Name"] == name:
-            num = input('Enter new number of allowed people')
-            Stores[i]["Allowed_People"] = num
+    for i in range(3):
+        if obj['Stores'][i]["Name"] == name:
+            num = int(input('Enter new number of allowed people'))
+            obj['Stores'][i]["Allowed"] = num
+
 
 
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import Opening_Hours
 import module1
+import change_num_of_people
 
 def main():
     Opening_Hours.opening_hours()
     module1.print_data()
+    change_num_of_people.change_num_of_people()
     
 
 if __name__ == "__main__":
