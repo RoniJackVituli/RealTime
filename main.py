af10663f2124fdae4ca217abfe55386978d569dd
yuval1121 <56655907+yuval1121@users.noreply.github.com>
2020-11-30 13:47:38 +0200

    Added final touches

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import Opening_Hours
 import module1
 import change_num_of_people
-import module2
+import UserStory1614
 
 
 def main():
     Opening_Hours.opening_hours()
     module1.print_data()
     change_num_of_people.change_num_of_people()
-    module2.START()
     
     
 
