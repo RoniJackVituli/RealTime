ff444c2746bd0bbfc90265f3b8e5730ed3fcfe6b
eytanbab <73233191+eytanbab@users.noreply.github.com>
2020-12-04 22:39:22 +0200

    Update Opening_Hours.py

--- Opening_Hours.py
+++ Opening_Hours.py
@@ -1,10 +1,13 @@
 import module1
-num_of_stores = 3
+from Counter import stores_registered
 
 
+length = stores_registered()
 obj = module1.open_data()
 
+
 def opening_hours():
-    for i in range(num_of_stores):
+    for i in range(length):
         print('Opening hours of ' + obj['Stores'][i]['Name'] + ':')
         print((obj['Stores'][i]['Hours']))
+    return True
